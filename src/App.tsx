2696f52fcfe38fc072cd409363298e52ccca0fbf
TIHeitorDS <heitorclaudinods@gmail.com>
2024-02-10 09:49:51 -0300

    feat: filter notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/Logo.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/NoteCard";
@@ -10,6 +10,7 @@ interface Note {
 }
 
 export function App() {
+  const [search, setSearch] = useState("");
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStoreged = localStorage.getItem("notes");
 
@@ -34,6 +35,15 @@ export function App() {
     setNotes(notesArray);
   }
 
+  function onSearchNote(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  const filteredNotes =
+    search != ""
+      ? notes.filter((note) => note.content.includes(search))
+      : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="nlw expert" />
@@ -43,6 +53,7 @@ export function App() {
           type="text"
           placeholder="Busque em suas anotações..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={onSearchNote}
         />
       </form>
 
@@ -51,7 +62,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />;
         })}
       </div>
